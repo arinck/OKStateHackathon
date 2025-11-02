06d11ffdfa6fed8d88a45a1445633c675b07db69
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-11-01 21:23:55 -0500

    added secretVars.py with secretKey

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from routes import register_routes  # Import the register_routes function from routes.py
+from secretVars import secretKey
 
 app = Flask(__name__)
 #required for linkedin APi
-app.secret_key = 'your-secret-key'
+app.secret_key = secretKey
 
 # Register the routes with the Flask app
 register_routes(app)
